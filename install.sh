9a6c09f4c6ee5fcdcef1fffd0cf0663d6be6dd9e
autochemistry <47147415+atomse@users.noreply.github.com>
2019-10-17 19:37:21 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,9 +10,16 @@ fi
 
 
 version=$(nginx -v 2>&1 | cut -d '/' -f 2)
+if [[ "$version" ~= ' ' ]]; then
+  version=$(echo $version | cut -d ' ' -f 1)
+fi
+echo version: $version
+
+
+
 nginx_dir=nginx-${version}
 nginx_fname=nginx-${version}.tar.gz
-wget -c http://nginx.org/download/${nginx_fname}
+wget -4 -c http://nginx.org/download/${nginx_fname}
 tar zxf $nginx_fname
 
 
@@ -28,6 +35,25 @@ GeoIP-devel \
 gperftools-libs gperftools-devel \
 pcre-devel \
 openssl-devel
+
+
+sudo apt install -y libxml2
+sudo apt install -y libxml2-dev
+sudo apt install -y libxslt
+sudo apt install -y libxslt-dev
+sudo apt install -y libgd
+sudo apt install -y libgd-dev
+sudo apt install -y perl-ExtUtils-Embed
+sudo apt install -y libgeoip-dev
+sudo apt install -y gperftools-libs
+sudo apt install -y gperftools-dev
+sudo apt install -y pcre-dev
+sudo apt install -y libpcre3
+sudo apt install -y libpcre3-dev
+sudo apt install -y openssl
+sudo apt install -y libssl-dev
+sudo apt install -y zlib1g.dev
+
 
 
 
